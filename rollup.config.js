79c089ad0211b93c43c215af2911d967b99f13d0
looqey <smirnar@mail.ru>
2024-05-28 09:59:02 +0300

    Minify build leaving comments there

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,27 @@ import terser from "@rollup/plugin-terser";
 export default {
     input: 'src/index.mjs',
     output: [
-        { dir: "build", format: "cjs", entryFileNames: 'index.cjs.js'},
-        { dir: "build", format: "esm", entryFileNames: 'index.esm.mjs'},
+        {
+            dir: "build",
+            format: "cjs",
+            plugins: [
+                terser({
+                    format: {
+                        comments: "all"
+                    },
+                })],
+            entryFileNames: 'index.cjs.js'
+        },
+        {
+            dir: "build",
+            format: "esm",
+            plugins: [
+                terser({
+                    format: {
+                        comments: "all"
+                    }
+                })],
+            entryFileNames: 'index.esm.mjs'
+        },
     ]
 };
